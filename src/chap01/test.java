ddeee3f6bf6b2b5ddbae848c070982d9bb656e5b
dy <dy@DESKTOP-V6DIDM9>
2019-06-30 23:23:18 +0900

    java git test

---
+++ src/chap01/test.java
@@ -0,0 +1,25 @@
+package chap01;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Random;
+
+/*
+ *  이클립스 단축키 
+ *  Ctrl+/ : 한 줄 주석 설정 및 해제
+ *  Ctrl+Shift+/ : 여러 줄 주석 설정
+ *  Ctrl+Shift+\ : 여러 줄 주석 해제
+ *  Ctrl+Shift+o : 자동 import
+ */ 
+public class test {
+	public static void main(String[] args) {
+		System.out.println("주석문처리하기");
+		System.out.println("주석문처리하기");
+		System.out.println("test");
+		String str = new String("java");
+		str.length();
+		Random rand = null;
+		ArrayList list = null;
+		InputStream is = null;
+	}
+}
